2d6478695aad3842b4e73583c62988e7f8b6c3fb
Esteban Canales <estebancomoprogramador@gmail.com>
2026-05-28 20:01:32 -0600

    chore: swap Montserrat for Inter + IBM Plex Mono

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import "@fontsource/montserrat/400.css";
-import "@fontsource/montserrat/500.css";
-import "@fontsource/montserrat/600.css";
-import "@fontsource/montserrat/700.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/500.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
